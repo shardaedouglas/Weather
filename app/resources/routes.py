0b3f3dbea353b078e333811dde4bf175cee1bf60
onyx.imeh <onyx.imeh@noaa.gov>
2024-11-05 18:21:03 +0000

    Merge O.Imeh features to ApplicationBaseline

--- app/resources/routes.py
+++ app/resources/routes.py
@@ -1,17 +1,18 @@
 from app.resources import resources_bp
+from flask import render_template
 
 
 @resources_bp.route('/resources/readme')
 def readme():
-    return '<h1>README</h1>' # Link html templates here instead..
+    return render_template('resources/readme.html')
 
 @resources_bp.route('/resources/country_codes')
 def country_codes():
-    return '<h1>Country Codes</h1>' # Link html templates here instead..
+    return render_template('resources/country_codes.html')
 
 @resources_bp.route('/resources/class_definitions')
 def class_definitions():
-    return '<h1>Class Definitions</h1>' # Link html templates here instead..
+    return render_template('resources/class_definitions.html')
 
 @resources_bp.route('/resources/accessibility')
 def accessibility():
